1e25db8ea64a0ae2599866354e54b1a025e6af5e
Stewart Mbofana <stewart@itopia.co.za>
2015-09-15 16:19:20 +0200

    Add and implement the Services project

---
+++ HomeCinema.Services/Abstract/IEncryptionService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeCinema.Services.Abstract
+{
+    interface IEncryptionService
+    {
+        string CreateSalt();
+        string EncryptPassword(string password, string salt);
+    }
+}
